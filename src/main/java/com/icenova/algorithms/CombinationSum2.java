360a92beb597f6d6e8a13d39336f64d59cccc633
Ilia Griaznov <i.b.gryaznov@gmail.com>
2025-04-04 22:33:28 +0300

    CombinationSum2
    Signed-off-by: Ilya Gryaznov <ilya.gryaznov@mvideo.ru>

---
+++ src/main/java/com/icenova/algorithms/CombinationSum2.java
@@ -0,0 +1,41 @@
+package com.icenova.algorithms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CombinationSum2 {
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> res = new ArrayList<>();
+
+        dfs(candidates, target, 0, new ArrayList<>(), res);
+        return res;
+    }
+
+    private void dfs(int[] candidates, int target, int start, List<Integer> comb, List<List<Integer>> res) {
+        if (target < 0) {
+            return;
+        }
+
+        if (target == 0) {
+            res.add(new ArrayList<>(comb));
+            return;
+        }
+
+        for (int i = start; i < candidates.length; i++) {
+            if (i > start && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+
+            if (candidates[i] > target) {
+                break;
+            }
+
+            comb.add(candidates[i]);
+            dfs(candidates, target - candidates[i], i + 1, comb, res);
+            comb.remove(comb.size() - 1);
+        }
+    }
+}
